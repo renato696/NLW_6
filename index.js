03d6d47977fbbf276e4929fbd62ede8abb81e73a
Renato Pires <renato696@gmail.com>
2021-06-21 06:36:48 -0300

    inicio do projeto
    deleto os arquivos index.ts/js, crio a pasta src, e o arquivo server.ts

--- index.js
+++
@@ -1,5 +0,0 @@
-function enviarEmail(nome, email) {
-    console.log("Enviar email para " + nome + " com email " + email);
-}
-;
-enviarEmail("Renato", "renato696@gmail.com");
--- index.ts
+++
@@ -1,5 +0,0 @@
-function enviarEmail(nome: string, email: string) {
-    console.log(`Enviar email para ${nome} com email ${email}`)
-};
-
-enviarEmail("Renato", "renato696@gmail.com");
