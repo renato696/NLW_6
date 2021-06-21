389a1cc7a96ded1c1728ff7c9dbf9e1b7a1e8e46
Renato Pires <renato696@gmail.com>
2021-06-21 06:46:11 -0300

    inicializando o servidor
    importo o express; jogo para a memoria: const app; crio o comando para definir a porta app.listen; e o console.log confirma

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,7 @@
 import express from "express";
 
-// @types/express - bibliotecas de tipagem - devem ser instaladas com o comando yarn add @types/express -D
+// @types/express - bibliotecas de tipagem - devem ser instaladas com o comando "yarn add @types/express -D" desta forma os 3 pontinhos embaixo do from "expresse" desaparece e está tudo ok para continuar.
 const app = express();
+
+// http://localhost:3000 - porta que vai estar o projeto
+app.listen(3000,() => console.log("Server is running at 3000"));
