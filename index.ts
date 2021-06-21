170b2d5149f9a282671957b99f56743b4ee035b6
Renato Pires <renato696@gmail.com>
2021-06-21 06:30:06 -0300

    inicializo o Typescript
    a função nao roda "node index.ts" porque o typescript não foi inializado. yarn tsc --int
    alterar no tsconfig.json o "strict: false"

--- index.ts
+++ index.ts
@@ -2,4 +2,4 @@ function enviarEmail(nome: string, email: string) {
     console.log(`Enviar email para ${nome} com email ${email}`)
 };
 
-enviarEmail("Renato", "renato@gmail.com");
+enviarEmail("Renato", "renato696@gmail.com");
