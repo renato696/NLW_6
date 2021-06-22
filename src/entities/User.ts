ca72d485154b1dcb570bf74fadddc868292907b1
Renato Pires <renato696@gmail.com>
2021-06-22 19:50:38 -0300

    insercao dos campos
    inseri os campos da tabela

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -1,6 +1,19 @@
 import {Entity} from "typeorm";
 
 @Entity("users")
-class User {}
+class User {
+
+    id: string;
+
+    name: string;
+
+    email: string;
+
+    admin: boolean;
+
+    created_at: Date;
+
+    updated_at: Date;
+}
 
 export { User };
