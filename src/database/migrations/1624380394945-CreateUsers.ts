b0bb4b355307de824fdc790d779385d59fe452c9
Renato Pires <renato696@gmail.com>
2021-06-22 13:59:53 -0300

    criação da primeira tabela usuario
    criação da primeira tabela.

--- src/database/migrations/1624380394945-CreateUsers.ts
+++ src/database/migrations/1624380394945-CreateUsers.ts
@@ -1,8 +1,43 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import {MigrationInterface, QueryRunner, Table, TreeLevelColumn} from "typeorm";
 
 export class CreateUsers1624380394945 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: "users",
+                columns: [
+                    {
+                        name: "id",
+                        type: "uuid",
+                        isPrimary: true,
+                    },
+                    {
+                        name: "name",
+                        type: "varchar",
+                    },
+                    {
+                        name: "mail",
+                        type: "varchar",
+                    },
+                    {
+                        name: "admin",
+                        type: "boolean",
+                        default: false
+                    },
+                    {
+                        name: "created_at",
+                        type: "timestamp",
+                        default: "now()"
+                    },
+                    {
+                        name: "updated_at",
+                        type: "timestamp",
+                        default: "now()"
+                    }
+                ]
+            })
+        )
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
