50db2a9c9f682a0bdc6ab59b078f215e7925a3ce
Renato Pires <renato696@gmail.com>
2021-06-22 20:02:37 -0300

    Constructor
    criação do construtor. sempre que tiver novo usuario, ele da um novo uuid

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -21,6 +21,12 @@ class User {
 
     @UpdateDateColumn()
     updated_at: Date;
+
+    constructor() {
+        if(!this.id) {
+            this.id = uuid();
+        }
+    }
 }
 
 export { User };
