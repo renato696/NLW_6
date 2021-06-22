a151fb36641a0fe6ebf580c4d1d1d72133bdc221
Renato Pires <renato696@gmail.com>
2021-06-22 19:54:40 -0300

    criação das colunas da tabela
    colunas da tabela

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -1,18 +1,24 @@
-import {Entity} from "typeorm";
+import {Entity, PrimaryColumn, Column, CreateDateColumn, UpdateDateColumn} from "typeorm";
 
 @Entity("users")
 class User {
 
-    id: string;
+    @PrimaryColumn()
+    readonly id: string;
 
+    @Column()
     name: string;
 
+    @Column()
     email: string;
 
+    @Column()
     admin: boolean;
 
+    @CreateDateColumn()
     created_at: Date;
 
+    @UpdateDateColumn()
     updated_at: Date;
 }
 
