1f352e0938203d7c039d596293be3f62abf41bc3
Renato Pires <renato696@gmail.com>
2021-06-23 05:03:42 -0300

    criação das primeiras regras
    Criação das regras para criar usuário:
    - verifica se já existe o email
    - se sim da erro
    - se não, cria o usuário

--- src/services/CreateUserService.ts
+++ src/services/CreateUserService.ts
@@ -1,8 +1,34 @@
+import { UsersRepositories } from "../repositories/UsersRepositories";
 
-
+interface IUserRequest {
+    name: string;
+    email: string;
+    admin?: boolean
+}
 
 class CreateUserService {
 
+    async execute({ name, email, admin} : IUserRequest) {
+        const usersRepository = new UsersRepositories();
+
+        if(!email) {
+            throw new Error("Email incorrect")
+        };
+
+        const userAlreadyExists = await usersRepository.findOne({
+            email
+        });
+
+        if (userAlreadyExists) {
+            throw new Error("User Already Exists");
+        };
+
+        const user = usersRepository.create({
+            name,
+            email,
+            admin
+        });
+    }
 
 }
 
