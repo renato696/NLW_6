ed9a129cdcb202499a2b603ccf7b9f48919886ad
Renato Pires <renato696@gmail.com>
2021-06-23 05:12:28 -0300

    Criação do controler
    Criação da camada controler, que é intermediária entre a requisição do server e o services.

---
+++ src/controllers/CreateUserControllers.ts
@@ -0,0 +1,11 @@
+import { Request, Response } from "express";
+
+
+class CreateUserController {
+
+    async handle(request: Request, response: Response) {
+        
+    };
+}
+
+export { CreateUserController} ;
