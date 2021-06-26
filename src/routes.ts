792b99e73a5466e89593e192386f1e634217bfc2
Renato Pires <renato696@gmail.com>
2021-06-26 14:39:17 -0300

    middlewares nas rotas
    colocando o middleware na rota

--- src/routes.ts
+++ src/routes.ts
@@ -1,13 +1,15 @@
 import { Router } from "express";
 import { CreateUserController } from "./controllers/CreateUserControllers";
 import { CreateTagController } from "./controllers/CreateTagController";
+import { ensureAdmin } from "./middlewares/ensureAdmin";
 
 const router = Router();
 
 const createUserController = new CreateUserController();
 const creatTagControler = new CreateTagController();
 
+router.post("/tags", ensureAdmin, creatTagControler.handle);
 router.post("/users", createUserController.handle);
-router.post("/tags", creatTagControler.handle);
+
 
 export { router };
