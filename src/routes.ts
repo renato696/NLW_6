0b76f75d93daab33e44f5a26e60d61b60547656b
Renato Pires <renato696@gmail.com>
2021-06-26 12:34:49 -0300

    colocando as Tags na rota
    Inclusão das Tags na Rota

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,13 @@
 import { Router } from "express";
 import { CreateUserController } from "./controllers/CreateUserControllers";
+import { CreateTagController } from "./controllers/CreateTagController";
 
 const router = Router();
 
 const createUserController = new CreateUserController();
+const creatTagControler = new CreateTagController();
 
 router.post("/users", createUserController.handle);
+router.post("/tags", creatTagControler.handle);
 
 export { router };
