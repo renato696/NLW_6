5c544202ff625f93c4d654dc27a21ec0e68a3b82
Renato Pires <renato696@gmail.com>
2021-06-27 20:17:28 -0300

    validar se token é valido
    validar se o token ainda é valido na aplicação.

--- src/middlewares/ensureAuthenticated.ts
+++ src/middlewares/ensureAuthenticated.ts
@@ -1,20 +1,28 @@
 import { Request, Response, NextFunction } from "express";
+import { verify } from "jsonwebtoken";
 
 export function ensureAuthenticated(request: Request, response: Response, next: NextFunction) {
     
     // receber o token
-    const token = request.headers.authorization;
+    const authToken = request.headers.authorization;
     
     // validar se token está preenchido
-    if(!token) {
+    if(!authToken) {
         return response.status(401).end();
-    }
-
-    return next();
-
+    };
 
     // validar se token é valido
+    const [,token] = authToken.split(" ");
+
+    try {
+        const decode = verify(token, "3f843693dbbfbaaed40b216f68f8a024");
+        console.log(decode);
+    }catch(err) {
+        return response.status(401).end();
+    };
 
     // recuperar informações do usuário
+    
 
+return next();
 }
