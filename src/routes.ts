e4d2ea8bf0b4c20e146c8715f174b8a7e3852f59
Renato Pires <renato696@gmail.com>
2021-06-27 10:42:05 -0300

    inclusão da rota login
    inclusão da rota AuthenticateUserController / login no routes

--- src/routes.ts
+++ src/routes.ts
@@ -2,14 +2,17 @@ import { Router } from "express";
 import { CreateUserController } from "./controllers/CreateUserControllers";
 import { CreateTagController } from "./controllers/CreateTagController";
 import { ensureAdmin } from "./middlewares/ensureAdmin";
+import { AuthenticateUserController } from "./controllers/AuthenticateUserController";
 
 const router = Router();
 
 const createUserController = new CreateUserController();
 const creatTagControler = new CreateTagController();
+const authenticateUserController = new AuthenticateUserController();
 
 router.post("/tags", ensureAdmin, creatTagControler.handle);
 router.post("/users", createUserController.handle);
+router.post("/login", authenticateUserController.handle);
 
 
 export { router };
