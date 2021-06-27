2ee5f4025eaeda155de504d68801c0f1b6fbc788
Renato Pires <renato696@gmail.com>
2021-06-27 13:18:33 -0300

    bug
    bug

--- src/entities/Compliment.ts
+++ src/entities/Compliment.ts
@@ -12,23 +12,23 @@ class Compliment {
     @Column()
     user_sender: string;
 
-    @JoinColumn({name: "user_sender"})
+    @JoinColumn({ name: "user_sender" })
     @ManyToOne(() => User)
-    userSender: User
+    userSender: User;
 
     @Column()
     user_receiver: string;
 
     @JoinColumn({name: "user_receiver"})
     @ManyToOne(() => User)
-    userReceiver: User
+    userReceiver: User;
 
     @Column()
     tag_id: string;
 
     @JoinColumn({name: "tag_id"})
     @ManyToOne(() => Tag)
-    tag: tag;
+    tag: Tag;
 
     @Column()
     message: string;
