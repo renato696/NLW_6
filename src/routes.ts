b4ca5fc7a4df4343e246e407df097298783e6bb2
Renato Pires <renato696@gmail.com>
2021-06-27 13:08:35 -0300

    rota Compliments
    inserindo a rota compliments e rodando a aplicação

--- src/routes.ts
+++ src/routes.ts
@@ -3,16 +3,18 @@ import { CreateUserController } from "./controllers/CreateUserControllers";
 import { CreateTagController } from "./controllers/CreateTagController";
 import { ensureAdmin } from "./middlewares/ensureAdmin";
 import { AuthenticateUserController } from "./controllers/AuthenticateUserController";
+import { CreateComplimentControler } from "./controllers/CreateComplimentController";
 
 const router = Router();
 
 const createUserController = new CreateUserController();
 const creatTagControler = new CreateTagController();
 const authenticateUserController = new AuthenticateUserController();
+const createComplimentController = new CreateComplimentControler();
 
 router.post("/tags", ensureAdmin, creatTagControler.handle);
 router.post("/users", createUserController.handle);
 router.post("/login", authenticateUserController.handle);
-
+router.post("/compliments", createComplimentController.handle);
 
 export { router };
