56550ae8f3311f24146cb72602b56eb63644b696
Renato Pires <renato696@gmail.com>
2021-06-27 20:54:34 -0300

    recupera as informações do usuario

--- src/middlewares/ensureAdmin.ts
+++ src/middlewares/ensureAdmin.ts
@@ -1,10 +1,16 @@
 import { Request, Response, NextFunction } from "express";
+import { getCustomRepository } from "typeorm";
+import { UsersRepositories } from "../repositories/UsersRepositories";
 
 
 
-export function ensureAdmin(request: Request, response: Response, next: NextFunction) {
+export async function ensureAdmin(request: Request, response: Response, next: NextFunction) {
     const { user_id } = request;
     console.log(user_id);
+
+    const usersRepositories = getCustomRepository(UsersRepositories);
+
+    const user = await usersRepositories.findOne(user_id)
 
     //verificar se usuario é admin
     const admin = true;
