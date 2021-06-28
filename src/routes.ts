dcc7a2b629b9f3e284a333197560bbe2511c2144
Renato Pires <renato696@gmail.com>
2021-06-27 21:48:44 -0300

    incluindo tag GET
    embora eu tenha duas rotas com o mesmo nome, elas possuem metodos diferentes: POST e GET

--- src/routes.ts
+++ src/routes.ts
@@ -7,23 +7,26 @@ import { AuthenticateUserController } from "./controllers/AuthenticateUserContro
 import { CreateComplimentController } from "./controllers/CreateComplimentController";
 import { ListUserSendComplimentsController } from "./controllers/ListUserSendComplimentsController";
 import { ListUserReceiveComplimentsController } from "./controllers/ListUserReceiveComplimentsController";
+import { ListTagsController } from "./controllers/ListTagsController";
 
 
 const router = Router();
 
 const createUserController = new CreateUserController();
-const creatTagControler = new CreateTagController();
+const creatTagController = new CreateTagController();
 const authenticateUserController = new AuthenticateUserController();
 const createComplimentController = new CreateComplimentController();
 const listUserSendComplimentsController = new ListUserSendComplimentsController();
 const listUserReceiveComplimentsController = new ListUserReceiveComplimentsController();
+const listTagsController = new ListTagsController();
 
-router.post("/tags", ensureAuthenticated, ensureAdmin, creatTagControler.handle);
+router.post("/tags", ensureAuthenticated, ensureAdmin, creatTagController.handle);
 router.post("/users", createUserController.handle);
 router.post("/login", authenticateUserController.handle);
 router.post("/compliments", ensureAuthenticated, createComplimentController.handle);
 
 router.get("/users/compliments/send", ensureAuthenticated ,listUserSendComplimentsController.handle);
 router.get("/users/compliments/receive", ensureAuthenticated ,listUserReceiveComplimentsController.handle);
+router.get("/tags", listTagsController.handle);
 
 export { router };
