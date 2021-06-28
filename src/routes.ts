7a8afa490390a54a7d99d24ea070ba652caf1655
Renato Pires <renato696@gmail.com>
2021-06-27 21:31:26 -0300

    inserindo as listas na rota

--- src/routes.ts
+++ src/routes.ts
@@ -5,6 +5,9 @@ import { ensureAuthenticated} from "./middlewares/ensureAuthenticated";
 import { ensureAdmin } from "./middlewares/ensureAdmin";
 import { AuthenticateUserController } from "./controllers/AuthenticateUserController";
 import { CreateComplimentController } from "./controllers/CreateComplimentController";
+import { ListUserSendComplimentsController } from "./controllers/ListUserSendComplimentsController";
+import { ListUserReceiveComplimentsController } from "./controllers/ListUserReceiveComplimentsController";
+
 
 const router = Router();
 
@@ -12,10 +15,15 @@ const createUserController = new CreateUserController();
 const creatTagControler = new CreateTagController();
 const authenticateUserController = new AuthenticateUserController();
 const createComplimentController = new CreateComplimentController();
+const listUserSendComplimentsController = new ListUserSendComplimentsController();
+const listUserReceiveComplimentsController = new ListUserReceiveComplimentsController();
 
 router.post("/tags", ensureAuthenticated, ensureAdmin, creatTagControler.handle);
 router.post("/users", createUserController.handle);
 router.post("/login", authenticateUserController.handle);
 router.post("/compliments", ensureAuthenticated, createComplimentController.handle);
+
+router.get("/users/compliments/send", listUserSendComplimentsController.handle);
+router.get("/users/compliments/receive", listUserReceiveComplimentsController.handle);
 
 export { router };
